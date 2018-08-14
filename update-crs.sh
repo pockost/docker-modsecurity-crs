f53a12fadce432943de73f486ad4a4b09eee7fc5
Romain THERRAT <romain42@gmail.com>
2018-08-14 15:15:10 +0200

    feat(crs): Add UPDATE_CRS env var

--- update-crs.sh
+++ update-crs.sh
@@ -3,5 +3,14 @@ set -e
 
 cd /workdir
 
-./util/upgrade.py --crs --geoip
-cp /workdir/crs-setup.conf.example /workdir/rules/crs-setup.conf
+UPDATE_CRS=${UPDATE_CRS:-"false"}
+
+./util/upgrade.py --geoip
+
+if [ "$UPDATE_CRS" == "true" ]
+then
+  ./util/upgrade.py --crs
+fi
+
+
+cp /workdir/crs-setup.conf.example /workdir/rules/00-crs-setup.conf
